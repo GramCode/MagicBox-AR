f0f3dc8aa3ad463068da27bd09da43bad9e0c9e1
Miguel Pacheco <miguel95_angel@hotmail.com>
2024-01-25 16:45:01 -0400

    Placing Objects in the World

---
+++ Assets/Scripts/AnimationStateManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateManager : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _anim;
+
+    private static int _randomNum;
+
+    private void Start()
+    {
+        _randomNum ++;
+
+        if (_randomNum > 3)
+        {
+            _randomNum = 0;
+        }
+
+        AnimationToPlay(_randomNum);
+    }
+
+    private void AnimationToPlay(int index)
+    {
+        _anim.SetInteger("State", index);
+    }
+}
---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody _rigid;
+    [SerializeField]
+    private float _jumpForce = 4f;
+
+    void Start()
+    {
+        CubeBehavior();
+    }
+
+    private void CubeBehavior()
+    {
+        _rigid.AddForce(Vector2.up * _jumpForce * Time.deltaTime, ForceMode.Force);
+    }
+}
