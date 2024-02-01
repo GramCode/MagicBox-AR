272ee8c006252a86c1b4737d506ba31418678c26
Miguel Pacheco <miguel95_angel@hotmail.com>
2024-01-31 22:19:13 -0400

    Assets as Placement Interactables

---
+++ Assets/Scripts/PlacementInteractableSelector.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.AR;
+
+public class PlacementInteractableSelector : MonoBehaviour
+{
+    [SerializeField]
+    private ARPlacementInteractable _arPlacementInteractable;
+    [SerializeField]
+    private GameObject[] _objectsToPlace;
+
+    private static int _index;
+
+    private void Update()
+    {
+        if (Input.touchCount == 0)
+        {
+            _index++;
+
+            if (_index > _objectsToPlace.Length - 1)
+            {
+                _index = 0;
+            }
+
+            _arPlacementInteractable.placementPrefab = _objectsToPlace[_index];
+        }
+    }
+
+}
