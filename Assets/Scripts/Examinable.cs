93bc007b8dddf4e1f57a3fb8b245db7c9de65729
Miguel Pacheco <miguel95_angel@hotmail.com>
2024-02-05 12:32:18 -0400

    Using Touch to Manipulate the Object

--- Assets/Scripts/Examinable.cs
+++ Assets/Scripts/Examinable.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Examinable : MonoBehaviour
 {
+    [SerializeField]
+    private float _examineScaleOffset = 1;
+    [SerializeField]
+    private Transform _parent;
 
     public void RequestExamine()
     {
-        ExaminableManager.Instance.PerformExamine(this);
+        ExaminableManager.Instance.PerformExamine(this, _examineScaleOffset);
     }
 
     public void RequestUnexamine()
     {
-        ExaminableManager.Instance.PerformUnexamine();
+        ExaminableManager.Instance.PerformUnexamine(_parent);
     }
 }
--- Assets/Scripts/ExaminableManager.cs
+++ Assets/Scripts/ExaminableManager.cs
@@ -23,26 +23,56 @@ public class ExaminableManager : MonoBehaviour
 
     [SerializeField]
     private Transform _examineTarget;
+    [SerializeField]
+    private float _rotateSpeed = 1;
     private Examinable _currentExaminableObject;
     private Vector3 _startingPosition;
     private Quaternion _startingRotation;
+    private Vector3 _startingScale;
+    private bool _isExamining = false;
 
-    public void PerformExamine(Examinable examinable)
+    private void Update()
+    {
+        if (_isExamining == true)
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    _currentExaminableObject.transform.Rotate(touch.deltaPosition.x * _rotateSpeed, touch.deltaPosition.y * _rotateSpeed, 0);
+                }
+            }
+        }
+    }
+
+    public void PerformExamine(Examinable examinable, float examineScaleOffset)
     {
         _currentExaminableObject = examinable;
 
         _startingPosition = _currentExaminableObject.transform.position;
         _startingRotation = _currentExaminableObject.transform.rotation;
+        _startingScale = _currentExaminableObject.transform.localScale;
+
+        Vector3 offsetScale = _startingScale * examineScaleOffset;
+        _currentExaminableObject.transform.localScale = offsetScale;
 
         _currentExaminableObject.transform.position = _examineTarget.transform.position;
-        _currentExaminableObject.transform.parent = _examineTarget;        
+        _currentExaminableObject.transform.parent = _examineTarget;
+
+        _isExamining = true;
     }
 
-    public void PerformUnexamine()
+    public void PerformUnexamine(Transform parent)
     {
         _currentExaminableObject.transform.position = _startingPosition;
         _currentExaminableObject.transform.rotation = _startingRotation;
-        _currentExaminableObject.transform.parent = null;
+        _currentExaminableObject.transform.localScale = _startingScale;
+        _currentExaminableObject.transform.parent = parent;
+        _currentExaminableObject = null;
+
+        _isExamining = false;
     }
 
 }
